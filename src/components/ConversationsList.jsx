4a4a452b96dc717c46b71f1c499ff9789a174345
Rom <romainprevostdev@gmail.com>
2022-07-01 11:19:07 +0200

    reworking conversations list

--- src/components/ConversationsList.jsx
+++ src/components/ConversationsList.jsx
@@ -11,46 +11,46 @@ const ConversationsList = ({ conversations, nickname, userId }) => {
   const userNameShort = nickname.charAt(0);
 
   return (
-    <>
-      {conversations.map((conversation) => (
-        <div key={conversation.id} className={styles.conversation}>
-          <div className={styles.conversationHeader}>
-            <span className={`${styles.avatar} ${styles[id]}`}>
-              {userNameShort}
-            </span>
-            <div className={styles.conversationDescription}>
+    <div className={styles.conversation}>
+      <div className={styles.conversationHeader}>
+        <span className={`${styles.avatar} ${styles[id]}`}>
+          {userNameShort}
+        </span>
+        <div className={styles.conversationDescription}>
+          <p>
+            {conversations.length > 1 ? 'Conversations' : 'Conversation'} de{' '}
+            <strong>{nickname}</strong>
+          </p>
+        </div>
+      </div>
+
+      <div className={styles.conversationContent}>
+        {conversations.map((conversation) => (
+          <div key={conversation.id}className={styles.content}>
+            <div>
               <p>
-                {conversations.length > 1 ? 'Conversations' : 'Conversation'} de{' '}
-                <strong>{nickname}</strong>
+                Avec <strong>{conversation.senderNickname}</strong>
+              </p>
+              <p>
+                Dernier message reçu le{' '}
+                <strong>
+                  {moment(conversation.lastMessageTimestamp, 'X').format(
+                    'DD MMMM'
+                  )}
+                </strong>
               </p>
             </div>
-          </div>
-
-          <div className={styles.conversationContent}>
             <div>
-              <div className={styles.content}>
-                <p>
-                  Avec <strong>{conversation.senderNickname}</strong>
-                </p>
-                <p>
-                  Dernier message reçu le{' '}
-                  <strong>
-                    {moment(conversation.lastMessageTimestamp, 'X').format(
-                      'DD MMMM'
-                    )}
-                  </strong>
-                </p>
-              </div>
-            </div>
-            <div>
-              <Link href={`/discussion/${nickname}/${userId}/${conversation.id}`}>
+              <Link
+                href={`/discussion/${nickname}/${userId}/${conversation.id}`}
+              >
                 <button>Voir la conversation</button>
               </Link>
             </div>
           </div>
-        </div>
-      ))}
-    </>
+        ))}
+      </div>
+    </div>
   );
 };
 

4e5698c407c235e4c148796b58a6cea4e63a1142
Rom <romainprevostdev@gmail.com>
2022-07-03 11:38:04 +0200

    changing App testing

--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -1,11 +1,11 @@
 import { render, screen } from "@testing-library/react"
-import App from "../pages"
+import Conversations from '../components/Conversations';
 
 describe("App", () => {
-  it("should render correctly App", () => {
-    render(<App />)
+  it("should render correctly Conversations parent component", () => {
+    render(<Conversations allUsers={[]} />)
     expect(
-      screen.getByText(/Welcome/)
+      screen.getByText(/Créer/)
     ).toBeInTheDocument()
   })
 })
